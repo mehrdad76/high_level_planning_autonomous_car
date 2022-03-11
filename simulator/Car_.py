33aff548b2c030a97f98dc74d1c21f5b6202ee0b
mehrdad76 <mehrdadkarrabi1997@gmail.com>
2022-03-11 19:24:08 +0100

    laser os strait and right segment completed

---
+++ simulator/Car_.py
@@ -0,0 +1,241 @@
+import math
+import numpy as np
+from Geometry import Point, LineSegment
+import Geometry
+
+MAX_LASER_DISTANCE = 5
+
+
+def get_in_range_angle(angle):
+    angle = angle % 360
+    if angle > 180:
+        return angle - 360
+    return angle
+
+
+class IntersectToConnectionLine:
+
+    def __init__(self, relative_point, relative_angle, connection_number, remaining_laser):
+        self.relative_point = relative_point
+        self.relative_angle = relative_angle
+        self.connection_number = connection_number
+        self.remaining_laser = remaining_laser
+
+    def __str__(self):
+        return 'relative point: ' + str(self.relative_point) + ', remaining laser: ' + str(self.remaining_laser)
+
+
+class Segment:
+
+    def __init__(self, segment_angle, number_of_connections):
+        self.number_of_connections = number_of_connections
+        self.segment_angle = segment_angle
+
+    def scan_lidar_one_laser(self, relative_position, relative_angle, max_laser_distance):
+        raise NotImplementedError("Please Implement this method")
+
+    def step(self):
+        raise NotImplementedError("Please Implement this method")
+
+    def scan_lidar(self, relative_car_position, number_of_rays, car_relative_angle, viewing_angle_range,
+                   max_laser_distance):  # viewing_angle_range is a number e.g. 45
+        ray_relative_angles = np.linspace(car_relative_angle - viewing_angle_range,
+                                          car_relative_angle + viewing_angle_range, number_of_rays)
+        ray_relative_angles = [get_in_range_angle(x_) for x_ in ray_relative_angles]
+        return [(-car_relative_angle + r_, self.scan_lidar_one_laser(relative_car_position, r_, max_laser_distance))
+                for r_ in ray_relative_angles]
+
+
+class StraitSegment(Segment):
+
+    def __init__(self, length, width, global_segment_angle):
+        super().__init__(global_segment_angle, 2)
+        self.length = length
+        self.width = width
+
+    def scan_lidar_one_laser(self, relative_position, relative_angle, max_laser_distance):
+        relative_angle = get_in_range_angle(relative_angle)
+        relative_x, relative_y = relative_position.x, relative_position.y
+        assert 0 <= relative_y <= self.width and 0 <= relative_x <= self.length
+        if relative_angle == 0:
+            distance_to_right = self.length - relative_x
+            if distance_to_right > max_laser_distance:
+                return None
+            return IntersectToConnectionLine(Point(self.length, relative_y), relative_angle,
+                                             1, max_laser_distance - distance_to_right)
+        if relative_angle == 180:
+            if relative_x > max_laser_distance:
+                return None
+            return IntersectToConnectionLine(Point(0, relative_y), relative_angle,
+                                             0, max_laser_distance - relative_x)
+        if relative_angle == 90:
+            distance_to_top = self.width - relative_y
+            if distance_to_top > max_laser_distance:
+                return None
+            return distance_to_top
+        if relative_angle == -90:
+            if relative_y > max_laser_distance:
+                return None
+            return relative_y
+        if -180 < relative_angle < -90:
+            laser_to_horizontal = relative_y / abs(math.sin(math.radians(relative_angle)))
+            laser_to_vertical = relative_x / abs(math.cos(math.radians(relative_angle)))
+        elif -90 < relative_angle < 0:
+            laser_to_horizontal = relative_y / abs(math.sin(math.radians(relative_angle)))
+            laser_to_vertical = (self.length - relative_x) / abs(math.cos(math.radians(relative_angle)))
+        elif 0 < relative_angle < 90:
+            laser_to_horizontal = (self.width - relative_y) / abs(math.sin(math.radians(relative_angle)))
+            laser_to_vertical = (self.length - relative_x) / abs(math.cos(math.radians(relative_angle)))
+        else:
+            laser_to_horizontal = (self.width - relative_y) / abs(math.sin(math.radians(relative_angle)))
+            laser_to_vertical = relative_x / abs(math.cos(math.radians(relative_angle)))
+        if min(laser_to_vertical, laser_to_horizontal) > max_laser_distance:
+            return None
+        if laser_to_horizontal <= laser_to_vertical:
+            return laser_to_horizontal
+        if -180 < relative_angle < -90:
+            return IntersectToConnectionLine(
+                Point(0, relative_y - laser_to_vertical * abs(math.sin(math.radians(relative_angle)))),
+                relative_angle, 0, max_laser_distance - laser_to_vertical)
+        elif -90 < relative_angle < 0:
+            return IntersectToConnectionLine(
+                Point(self.length, relative_y - laser_to_vertical * abs(math.sin(math.radians(relative_angle)))),
+                relative_angle, 1, max_laser_distance - laser_to_vertical)
+        elif 0 < relative_angle < 90:
+            return IntersectToConnectionLine(
+                Point(self.length, relative_y + laser_to_vertical * abs(math.sin(math.radians(relative_angle)))),
+                relative_angle, 1, max_laser_distance - laser_to_vertical)
+        else:
+            return IntersectToConnectionLine(
+                Point(0, relative_y + laser_to_vertical * abs(math.sin(math.radians(relative_angle)))),
+                relative_angle, 0, max_laser_distance - laser_to_vertical)
+
+    def step(self):
+        pass
+
+
+class RightTurnSegment(Segment):
+
+    def __init__(self, length_of_smaller_wall, width, alpha, global_segment_angle):
+        super().__init__(global_segment_angle, 2)
+        self.length_of_smaller_wall = length_of_smaller_wall
+        self.width = width
+        alpha = math.radians(alpha)
+        self.alpha = alpha
+
+        self.relative_vertices = []
+        self.relative_vertices.append(Point(0, 0))
+        self.relative_vertices.append(Point(width, 0))
+        self.relative_vertices.append(Point(width, length_of_smaller_wall))
+        self.relative_vertices.append(Point(width + length_of_smaller_wall * math.sin(alpha),
+                                            length_of_smaller_wall - length_of_smaller_wall * math.cos(alpha)))
+        tmp_point = Point(0, length_of_smaller_wall + width / math.tan(alpha / 2))
+        self.relative_vertices.append(Point(tmp_point.y * math.sin(alpha),
+                                            tmp_point.y - tmp_point.y * math.cos(alpha)))
+        self.relative_vertices.append(tmp_point)
+
+        self.relative_main_line_segments = []
+        self.relative_main_line_segments.append(LineSegment(self.relative_vertices[1], self.relative_vertices[2]))
+        self.relative_main_line_segments.append(LineSegment(self.relative_vertices[2], self.relative_vertices[3]))
+        self.relative_main_line_segments.append(LineSegment(self.relative_vertices[4], self.relative_vertices[5]))
+        self.relative_main_line_segments.append(LineSegment(self.relative_vertices[5], self.relative_vertices[0]))
+
+        self.relative_connection_line_segments = []
+        self.relative_connection_line_segments.append(LineSegment(self.relative_vertices[0], self.relative_vertices[1]))
+        self.relative_connection_line_segments.append(LineSegment(self.relative_vertices[3], self.relative_vertices[4]))
+
+    def point_is_inside_or_on(self, point):
+        for line_segment in self.relative_main_line_segments:
+            if line_segment.is_on_segment(point):
+                return True
+        for line_segment in self.relative_connection_line_segments:
+            if line_segment.is_on_segment(point):
+                return True
+        tmp_segment = LineSegment(self.relative_vertices[2], self.relative_vertices[5])
+        if tmp_segment.is_on_segment(point):
+            return True
+        if Geometry.orientation(tmp_segment.start_point, tmp_segment.end_point, point) == -1:
+            if Geometry.orientation(self.relative_vertices[5], self.relative_vertices[0], point) == \
+                    Geometry.orientation(self.relative_vertices[0], self.relative_vertices[1], point) == \
+                    Geometry.orientation(self.relative_vertices[1], self.relative_vertices[2], point) == -1:
+                return True
+            return False
+        elif Geometry.orientation(tmp_segment.start_point, tmp_segment.end_point, point) == 1:
+            if Geometry.orientation(self.relative_vertices[5], self.relative_vertices[4], point) == \
+                    Geometry.orientation(self.relative_vertices[4], self.relative_vertices[3], point) == \
+                    Geometry.orientation(self.relative_vertices[3], self.relative_vertices[2], point) == 1:
+                return True
+            return False
+        return False
+
+    def scan_lidar_one_laser(self, relative_position, relative_angle, max_laser_distance):
+        assert self.point_is_inside_or_on(relative_position)
+        relative_angle = math.radians(get_in_range_angle(relative_angle))
+        laser = LineSegment(relative_position, Point(relative_position.x +
+                                                     max_laser_distance * math.cos(relative_angle),
+                                                     relative_position.y + max_laser_distance *
+                                                     math.sin(relative_angle)))
+        res = None
+        min_distance = math.inf
+        for line_segment in self.relative_main_line_segments:
+            intersection = line_segment.get_intersection_to(laser)
+            if intersection is None:
+                continue
+            distance = relative_position.get_distance_to(intersection)
+            if distance < min_distance:
+                min_distance = distance
+                res = distance
+        for index in range(2):
+            line_segment = self.relative_connection_line_segments[index]
+            intersection = line_segment.get_intersection_to(laser)
+            if intersection is None:
+                continue
+            distance = relative_position.get_distance_to(intersection)
+            if distance < min_distance:
+                min_distance = distance
+                res = IntersectToConnectionLine(intersection, relative_angle, index, max_laser_distance - distance)
+
+        if min_distance <= max_laser_distance:
+            return res
+        return None
+
+    def step(self):
+        pass
+
+
+class LeftTurnSegment(Segment):
+
+    def __init__(self, length_of_smaller_wall, width, alpha, global_segment_angle):
+        super().__init__(global_segment_angle, 2)
+        self.length_of_smaller_wall = length_of_smaller_wall
+        self.width = width
+        alpha = math.radians(alpha)
+        self.alpha = alpha
+
+    def scan_lidar_one_laser(self, relative_position, relative_angle, max_laser_distance):
+        pass
+
+    def step(self):
+        pass
+
+
+class SegmentHolder:
+
+    def __init__(self, segment):
+        self.segment = segment
+        self.connections = [(None, -1) for _ in range(segment.number_of_connections)]
+
+    def set_segment_connection(self, self_connection_number, other_segment_holder, other_connection_number):
+        self.connections[self_connection_number] = (other_segment_holder, other_connection_number)
+
+
+class Map:
+
+    # segments is a list of StraitSegment, ...
+    # connections is a list of Connection
+    def __init__(self, segment_holders):
+        self.segment_holders = segment_holders
+        self.check_map()
+
+    def check_map(self):
+        pass  # TODO: check if the map is well connected and widths are the same
---
+++ simulator/Geometry.py
@@ -0,0 +1,97 @@
+import math
+
+
+def orientation(p, q, r):
+    val = (float(q.y - p.y) * (r.x - q.x)) - (float(q.x - p.x) * (r.y - q.y))
+    if val > 0:
+        return 1
+    elif val < 0:
+        return -1
+    else:
+        return 0
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_tuple(self):
+        return self.x, self.y
+
+    def get_distance_to(self, point):
+        return math.sqrt((math.pow(self.y - point.y, 2)) + (math.pow(self.x - point.x, 2)))
+
+    def __str__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
+
+class LineSegment:
+
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def is_on_segment(self, point):
+        return ((point.y - self.start_point.y) * (self.end_point.x - self.start_point.x) ==
+                (point.x - self.start_point.x) * (self.end_point.y - self.start_point.y)) and \
+               ((self.start_point.x <= point.x <= self.end_point.x) or
+                (self.end_point.x <= point.x <= self.start_point.x)) and \
+               ((self.start_point.y <= point.y <= self.end_point.y) or
+                (self.end_point.y <= point.y <= self.start_point.y))
+
+    def collinear_on_segment(self, point):
+        if ((point.x <= max(self.start_point.x, self.end_point.x)) and (
+                point.x >= min(self.start_point.x, self.end_point.x)) and
+                (point.y <= max(self.start_point.y, self.end_point.y)) and (
+                        point.y >= min(self.start_point.y, self.end_point.y))):
+            return True
+        return False
+
+    def do_intersect_to(self, line_segment):
+        o1 = orientation(self.start_point, self.end_point, line_segment.start_point)
+        o2 = orientation(self.start_point, self.end_point, line_segment.end_point)
+        o3 = orientation(line_segment.start_point, line_segment.end_point, self.start_point)
+        o4 = orientation(line_segment.start_point, line_segment.end_point, self.end_point)
+
+        if (o1 != o2) and (o3 != o4):
+            return True
+
+        if (o1 == 0) and self.collinear_on_segment(line_segment.start_point):
+            return True
+
+        if (o2 == 0) and self.collinear_on_segment(line_segment.end_point):
+            return True
+
+        if (o3 == 0) and line_segment.collinear_on_segment(self.start_point):
+            return True
+
+        if (o4 == 0) and line_segment.collinear_on_segment(self.end_point):
+            return True
+
+        return False
+
+    def get_intersection_to(self, line_segment):
+        if not self.do_intersect_to(line_segment):
+            return None
+
+        xdiff = (self.start_point.x - self.end_point.x, line_segment.start_point.x - line_segment.end_point.x)
+        ydiff = (self.start_point.y - self.end_point.y, line_segment.start_point.y - line_segment.end_point.y)
+
+        def det(a, b):
+            return a[0] * b[1] - a[1] * b[0]
+
+        div = det(xdiff, ydiff)
+        if div == 0:
+            return None
+
+        d = (det(self.start_point.get_tuple(), self.end_point.get_tuple()),
+             det(line_segment.start_point.get_tuple(), line_segment.end_point.get_tuple()))
+        x = det(d, xdiff) / div
+        y = det(d, ydiff) / div
+        return Point(x, y)
+
+    def __str__(self):
+        return '(' + str(self.start_point) + ' -> ' + str(self.end_point) + ')'
+
